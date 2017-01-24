5f0787c9f1cf0fb3a1ea7963c8725446c3f64770
ja3ekja <jacekfeliksiak@gmail.com>
2017-01-24 10:46:45 +0100

    test

---
+++ src/main/java/com/epam/app/controllers/Controller.java
@@ -0,0 +1,16 @@
+package com.epam.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jacek on 24.01.17.
+ */
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Szafbook: work in progress!";
+    }
+}
---
+++ src/main/java/com/epam/app/main/App.java
@@ -0,0 +1,17 @@
+package com.epam.app.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Jacek Feliksiak
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class, args);
+
+    }
+}
