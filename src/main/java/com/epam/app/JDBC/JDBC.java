837d63c6860e06a4e70ecb88a2b0c722321c119b
ja3ekja <jacekfeliksiak@gmail.com>
2017-01-24 16:29:54 +0100

    db connection

---
+++ src/main/java/com/epam/app/JDBC/JDBC.java
@@ -0,0 +1,17 @@
+package com.epam.app.JDBC;
+
+/**
+ * Created by jacek on 24.01.17.
+ */
+public class JDBC {
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:oracle:thin@localhost:1521:xe";
+
+    static final String USER = "oracle";
+    static final String PASS = "oracle";
+
+    public static void connection() {
+
+    }
+}
